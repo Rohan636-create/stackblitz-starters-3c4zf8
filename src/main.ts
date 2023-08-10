ec68d48bd6e6d9bea5171f9cd6d01a19b5bbcc4e
Rohan636-create <141109390+Rohan636-create@users.noreply.github.com>
2023-08-10 13:34:10 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,9 @@ import { AppComponent } from './app/app.component';
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule,AppComponent],
+  imports: [CommonModule, AppComponent],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+  <app-aap></app-app>
   `,
 })
 export class App {
