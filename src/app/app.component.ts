b16d44a639801ff21c4f21584c3db4a816c89c7c
Rohan636-create <141109390+Rohan636-create@users.noreply.github.com>
2023-08-10 13:54:00 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title: string = ' hero app';
   user: string = 'rohan';
   phonenumber: string = '123456789';
   age: number = 20;
