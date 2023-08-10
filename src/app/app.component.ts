00a042a4633a515b67cedd195c2a9ceb6a1161b8
Rohan636-create <141109390+Rohan636-create@users.noreply.github.com>
2023-08-10 13:48:11 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  user: string = 'rohan';
+  phonenumber: string = '123456789';
+  age: number = 20;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
