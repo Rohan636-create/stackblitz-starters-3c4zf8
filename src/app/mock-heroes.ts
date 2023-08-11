aed67bee2907c7f057352ba2cdc81dde0aba66f1
Rohan636-create <141109390+Rohan636-create@users.noreply.github.com>
2023-08-11 10:35:24 +0530

    1 file modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,7 +1,10 @@
 import { Hero } from './hero';
 export const HEROES: Hero[] = [
-  { id: 12, name: virat },
-  { id: 13, name: dhoni },
-  { id: 14, name: rohit },
-  { id: 15, name: bhuvi },
+  { id: 12, name: 'virat' },
+  { id: 13, name: 'dhoni' },
+  { id: 14, name: 'rohit' },
+  { id: 15, name: 'bhuvi' },
+  { id: 16, name: 'siraj'},
+  { id: 17, name: 'yuzi'},
+  { id: 18,name: 'abd'},
 ];
