99734664740a0e54f415a78caed4d992b6565d55
Rohan636-create <141109390+Rohan636-create@users.noreply.github.com>
2023-08-11 10:41:56 +0530

    3 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
@@ -12,11 +12,12 @@ import { HEROES } from '../mock-heroes';
   imports: [CommonModule, FormsModule],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'rohan',
-  };
-  heroes = HEROES;
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'rohan',
+  // };
+  // heroes = HEROES;
+  @Input() hero!: Hero;
 
   constructor() {}
 
