b1f196b024e559692690c72ade010c5b4102a3c6
Rohan636-create <141109390+Rohan636-create@users.noreply.github.com>
2023-08-11 10:44:42 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HEROES } from './mock-heroes';
@@ -7,7 +8,7 @@ import { HEROES } from './mock-heroes';
   selector: 'app-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeroesComponent],
+  imports: [HeroesComponent, CommonModule],
 })
 export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
